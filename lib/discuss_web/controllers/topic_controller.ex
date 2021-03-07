284f4297930f3fa733b195daeadc684c16391e68
Jannik <jw19980309@gmail.com>
2021-03-07 10:30:12 +0100

    Added new Topic View

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,8 +1,11 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
+  alias Discuss.Topic
 
   @spec new(Plug.Conn.t(), any) :: Plug.Conn.t()
   def new(conn, _params) do
-    render(conn, "index.html")
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render(conn, "index.html", changeset: changeset)
   end
 end
