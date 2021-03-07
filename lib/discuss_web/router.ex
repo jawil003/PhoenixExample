f0ec16d2c62a02872c022d4bbd76ea5bc9ae6dc9
Jannik <jw19980309@gmail.com>
2021-03-07 15:38:50 +0100

    Add Floating Button

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -15,11 +15,9 @@ defmodule DiscussWeb.Router do
 
   scope "/", DiscussWeb do
     pipe_through :browser
-
-    get "/", PageController, :index
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
-    get "/topics", TopicController, :index
+    get "/", TopicController, :index
   end
 
   # Other scopes may use custom stacks.
