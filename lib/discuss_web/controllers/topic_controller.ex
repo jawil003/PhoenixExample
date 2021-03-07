6b0e698f3272b6e68e2473441316c9bb0c55e998
Jannik <jw19980309@gmail.com>
2021-03-07 09:50:31 +0100

    Create Topic Schema

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,6 +1,7 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
+  @spec new(Plug.Conn.t(), any) :: Plug.Conn.t()
   def new(conn, _params) do
     render(conn, "index.html")
   end
