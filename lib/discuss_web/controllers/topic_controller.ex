93a44a3e704f4666c58ca7e0bad124e50fee9c72
Jannik <jw19980309@gmail.com>
2021-03-07 13:55:57 +0100

    Added List on Index of Topics

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -3,19 +3,30 @@ defmodule DiscussWeb.TopicController do
   alias Discuss.Topic
   alias Discuss.Repo
 
+  def index(conn, params) do
+    topics = Repo.all(Topic)
+    render(conn, "index.html", topics: topics)
+  end
+
   @spec new(Plug.Conn.t(), any) :: Plug.Conn.t()
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
-    render(conn, "index.html", changeset: changeset)
+    render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, struct} -> IO.inspect(struct)
-      {:error, changeset} -> render(conn, "index.html", changeset: changeset)
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic created")
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, changeset} ->
+        conn
+        |> render("new.html", changeset: changeset)
     end
   end
 end
