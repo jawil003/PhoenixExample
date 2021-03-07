753f48484b4cd15e3dcde1d16b91f7f36d9a55bf
Jannik <jw19980309@gmail.com>
2021-03-07 13:01:19 +0100

    Updated Styles and Created Form Error Handler

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,11 +1,21 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
   alias Discuss.Topic
+  alias Discuss.Repo
 
   @spec new(Plug.Conn.t(), any) :: Plug.Conn.t()
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render(conn, "index.html", changeset: changeset)
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, struct} -> IO.inspect(struct)
+      {:error, changeset} -> render(conn, "index.html", changeset: changeset)
+    end
   end
 end
